14897a07adf8c50778ba8e211e9b769bc163a511
haru <ishiharuweb@gmail.com>
2019-01-19 13:05:19 +0000

    update tasks

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -41,16 +41,16 @@ class TasksController < ApplicationController
 
   def destroy
     @task = Task.find(params[:id])
-    @message.destroy
+    @task.destroy
     
     flash[:success] = 'Task は正常に削除されました'
-    redirect_to tasks_url
+    redirect_to tasks_path
   end
-end
+  
+  private
 
-private
-
-# Strong Parameter
-def task_params
-  params.require(:task).permit(:content)
+　# Strong Parameter
+　def task_params
+  　params.require(:task).permit(:content)
+　end
 end
