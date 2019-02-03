28f16087b7d5bd7d00895b5fa4c26fa36df531a3
haru <ishiharuweb@gmail.com>
2019-02-03 07:20:57 +0000

    post

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,6 +1,7 @@
 class TasksController < ApplicationController
-  before_action :set_task, only: [:show, :edit, :update, :destroy]
   before_action :require_user_logged_in
+  before_action :set_task, only: [:show, :edit, :update, :destroy]
+  before_action :correct_user, only: [:destroy]
   
   def index
     @tasks = Task.all.page(params[:page]).per(10)
@@ -54,5 +55,12 @@ class TasksController < ApplicationController
   # Strong Parameter
   def task_params
     params.require(:task).permit(:content, :status)
+  end
+  
+  def correct_user
+    @task = current_user.tasks.find_by(id: params[:id])
+    unless @task
+      redirect_to root_url
+    end
   end
 end
--- app/controllers/toppages_controller.rb
+++ app/controllers/toppages_controller.rb
@@ -1,4 +1,7 @@
 class ToppagesController < ApplicationController
   def index
+    if logged_in?
+      @tasks = Task.all.page(params[:page]).per(10)
+    end
   end
 end
